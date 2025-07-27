a88eb5035aee4c453c7d0d7662dd553c075f23b4
Waseemko-ayman <wasimabdelhadi78@gmail.com>
2025-07-27 11:55:27 +0300

    Assignment 8 completed

---
+++ submissions/WaseemAbdElhadi/assignment8.js
@@ -0,0 +1,50 @@
+// Task 1: Fix the Lost Context
+const editor = {
+  title: 'my first blog',
+  getUpperTitle() {
+    return this.title.toUpperCase();
+  }
+};
+
+const getTitle = editor.getUpperTitle();
+console.log(getTitle);
+
+// Write code that ensures getTitle() works as expected and logs "MY FIRST BLOG"
+
+// ---------------------------------------------------------------------------------
+// ---------------------------------------------------------------------------------
+// ---------------------------------------------------------------------------------
+
+// Task 2: Arrow or Regular ?
+const formHandler = {
+  value: 'initial',
+  onChange(newValue) {
+    this.value = newValue;
+  }
+};
+
+function simulateInputChange(callback) {
+  callback('updated');
+}
+
+simulateInputChange((value) => formHandler.onChange(value));
+console.log(formHandler.value)
+
+// ---------------------------------------------------------------------------------
+// ---------------------------------------------------------------------------------
+// ---------------------------------------------------------------------------------
+
+// Task 3: Method Extraction Issue
+
+const translator = {
+  language: 'Arabic',
+  getLanguage() {
+    return `Current language: ${this.language}`;
+  }
+};
+
+function logLanguageInfo(getter) {
+  console.log(getter())
+}
+
+logLanguageInfo(() => translator.getLanguage());
