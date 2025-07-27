8d2d159e2420fed94f70d9e064ee1666c75f9463
nora <noraashqar244@gmail.com>
2025-07-27 20:37:08 +0300

    Add solution

---
+++ submissions/NoraAshqar/assignment8.js
@@ -0,0 +1,54 @@
+//Task 1: Fix the Lost Context
+const editor = {
+  title: 'my first blog',
+  getUpperTitle() {
+    return this.title.toUpperCase();
+  }
+};
+
+// Write code that ensures getTitle() works as expected and logs "MY FIRST BLOG". Do not modify the editor object.
+
+const getTitle =editor.getUpperTitle.bind(editor);
+ console.log(getTitle());
+
+ //Task 2: Arrow or Regular?
+ const formHandler = {
+  value: 'initial',
+  onChange(newValue) {
+    this.value = newValue;
+  }
+};
+
+function simulateInputChange(callback) {
+  callback('updated');
+}
+
+simulateInputChange(formHandler.onChange);
+
+// ❓ Now log formHandler.value Your Tasks:
+console.log(formHandler.value);
+//Predict what formHandler.value will be.
+
+// formHandler.value will remain "initial"
+
+//Fix the bug using either .bind() or an arrow function (your choice).
+simulateInputChange(formHandler.onChange.bind(formHandler));
+
+//Explain which fix you used and why.
+
+// i used .bind() to make (this) points to formHandler cuz  when (on change)passed it lost where it should point (formHandler)
+
+
+//Task 3: Method Extraction Issue
+
+const translator = {
+  language: 'Arabic',
+  getLanguage() {
+    return `Current language: ${this.language}`;
+  }
+};
+// ❓ Write a function logLanguageInfo(getter) that logs the language info correctly even if getLanguage is passed as an argument.
+function logLanguageInfo(getter){
+console.log(getter.call(translator));
+}
+logLanguageInfo(translator.getLanguage);
